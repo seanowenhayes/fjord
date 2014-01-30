32ecf4e0f5a1d94ccb25b975406424d59df534c1
Will Kahn-Greene <willkg@mozilla.com>
2014-01-30 13:27:30 -0500

    Make run_l10n_completion script way more explicit

--- bin/run_l10n_completion.sh
+++ bin/run_l10n_completion.sh
@@ -2,20 +2,28 @@
 
 # Run this from the project root--not this directory!
 #
-# Usage: run_l10n_completion.sh [PYTHON-BIN]
+# Usage: ./bin/run_l10n_completion.sh [PYTHON-BIN]
 
-PYTHONBIN=$1
+function usage() {
+    echo "syntax: ./bin/run_l10n_completion.sh WEBAPP-DIR PYTHON-BIN"
+    exit 1
+}
+
+if [[ $# -lt 2 ]]; then
+    usage
+fi
+
+WEBAPP=$1
+PYTHONBIN=$2
 
 # check if file and dir are there
 if [[ ! -f "$PYTHONBIN" ]]; then 
-    PYTHONBIN=/usr/bin/python
+    usage
 fi
 
 # Update .po files in svn
-cd ./locale && svn up && cd ..
+cd $WEBAPP/locale && svn up && cd $WEBAPP
 
 # Run l10n completion to calculate percent completed and update .json
 # file; keep 90 days of data
-$PYTHONBIN ./bin/l10n_completion.py --truncate 90 ./media/l10n_completion.json ./locale/
-
-echo "Completed."
+$PYTHONBIN $WEBAPP/bin/l10n_completion.py --truncate 90 $WEBAPP/media/l10n_completion.json $WEBAPP/locale/
